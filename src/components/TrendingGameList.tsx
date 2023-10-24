b5f2f160ef68e8e79d68d41b8e139e737a728b53
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-24 14:43:05 +0300

    add: trnding games list

---
+++ src/components/TrendingGameList.tsx
@@ -0,0 +1,39 @@
+import { TrendingGameListProps } from '@/types';
+import Image from 'next/image';
+
+const TrendingGameList = ({ gameList }: TrendingGameListProps) => {
+    return (
+        <div className="hidden md:block mt-5">
+            <h2 className="font-bold text-[30px] dark:text-white">
+                Trending Games
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
+                {gameList.map(
+                    (item, index) =>
+                        index < 4 && (
+                            <div
+                                className="pb-14 hover:scale-110 transition-all relative duration-300 cursor-pointer"
+                                // onClick={() => getMovieDetails(item.id)}
+                                key={item.id}
+                            >
+                                <Image
+                                    src={item.background_image}
+                                    width={1080}
+                                    height={270}
+                                    className="w-full h-[270px] rounded-t-xl object-cover"
+                                    alt={item.slug}
+                                />
+                                <div className="p-2 h-[80px] w-full backdrop-blur-sm bg-[#76a8f75e] rounded-b-xl bg-gradient-to-b">
+                                    <h2 className="dark:text-white text-[20px] font-bold ">
+                                        {item.name}
+                                    </h2>
+                                </div>
+                            </div>
+                        )
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default TrendingGameList;
