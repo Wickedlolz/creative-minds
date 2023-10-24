1db43deae8e6c749589aa5b138b03498e80111c5
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-24 14:43:33 +0300

    add: home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import Image from "next/image";
+import { getGamesByGenreId, getPopularGames } from '@/utils';
 
-export default function Home() {
-  return <section>Home Page</section>;
+import PopularGameList from '@/components/PopularGameList';
+import SideNav from '@/components/SideNav';
+import Slider from '@/components/Slider';
+import TrendingGameList from '@/components/TrendingGameList';
+
+export default async function Home() {
+    const gameList = await getPopularGames();
+    const gamesListByGenre = await getGamesByGenreId(0);
+
+    return (
+        <div className="grid grid-cols-4 p-8">
+            <div className="hidden md:flex">
+                <SideNav
+                // selectedGenreId={(genreId) => getGameList(genreId)}
+                />
+            </div>
+            {gameList?.length && gamesListByGenre?.length > 0 ? (
+                <div className="md:col-span-3 col-span-4 px-3">
+                    <Slider game={gamesListByGenre[0]} />
+                    <TrendingGameList gameList={gameList} />
+                    <PopularGameList gameList={gamesListByGenre} />
+                </div>
+            ) : null}
+        </div>
+    );
 }
