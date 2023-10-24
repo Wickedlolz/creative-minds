402e8125a90b8db6de4f1982e85b558c9987407a
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-24 16:19:01 +0300

    add genre page

---
+++ src/app/genre/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { GenrePageProps } from '@/types';
+
+import AsideNav from '@/components/AsideNav';
+import PopularGameList from '@/components/PopularGameList';
+import Slider from '@/components/Slider';
+import TrendingGameList from '@/components/TrendingGameList';
+import { getGamesByGenreId, getPopularGames } from '@/utils';
+
+export default async function GenrePage({ params }: GenrePageProps) {
+    const { id } = params;
+    const gameList = await getPopularGames();
+    const gamesListByGenre = await getGamesByGenreId(Number(id));
+
+    return (
+        <>
+            {gameList?.length && gamesListByGenre?.length > 0 && (
+                <div className="md:col-span-3 col-span-4 px-3">
+                    <Slider game={gamesListByGenre[0]} />
+                    <TrendingGameList gameList={gameList} />
+                    <PopularGameList gameList={gamesListByGenre} />
+                </div>
+            )}
+        </>
+    );
+}
