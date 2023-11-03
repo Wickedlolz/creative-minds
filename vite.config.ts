89fe5c8bb94f291f3c6d4e6b5868d40085dd4dbd
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-03 17:01:13 +0200

    add configuration for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: 'prompt',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+        name: 'Creative Minds',
+        short_name: 'Creative Minds',
+        description: 'Place where you can share your thoughts.',
+        icons: [],
+        theme_color: '#171717',
+        background_color: '#e8ebf2',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: './',
+    plugins: [react(), VitePWA(manifestForPlugin)],
+});
