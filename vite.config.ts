c8123ad53a3f810526a858c6e60946ddeaddfe24
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-03 18:41:16 +0200

    finish PWA and add icons ✅

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
-    registerType: 'autoUpdate',
+    registerType: 'prompt',
     injectRegister: 'auto',
     workbox: {
         clientsClaim: true,
@@ -19,14 +19,24 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
         description: 'Place where you can share your thoughts.',
         icons: [
             {
-                src: '/vite.svg',
+                src: '/android-chrome-192x192.png',
                 sizes: '192x192',
-                type: 'image/svg',
+                type: 'image/png',
             },
             {
-                src: '/vite.svg',
-                sizes: '225x225',
-                type: 'image/svg',
+                src: '/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+            },
+            {
+                src: '/maskable_icon.png',
+                sizes: '512x512',
+                type: 'image/png',
                 purpose: 'any maskable',
             },
         ],
