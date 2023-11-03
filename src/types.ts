5f88ce2803037564d617dbd84cd902f90386cdcd
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-03 22:30:24 +0200

    remove unused type ❌

--- src/types.ts
+++ src/types.ts
@@ -1,51 +1,42 @@
-import { User, UserCredential } from "firebase/auth";
-import { DocumentData } from "firebase/firestore";
-import { ReactNode } from "react";
+import { User, UserCredential } from 'firebase/auth';
+import { DocumentData } from 'firebase/firestore';
+import { ReactNode } from 'react';
 
 export type LayoutProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 export type FirebaseProviderProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 export type FirebaseInitialState = {
-  user: User | null;
-  signInWithGoogle: () => Promise<UserCredential>;
-  logOut: () => Promise<void>;
+    user: User | null;
+    signInWithGoogle: () => Promise<UserCredential>;
+    logOut: () => Promise<void>;
 };
 
 export type MessageProps = {
-  children?: ReactNode;
-  post: Post | DocumentData;
+    children?: ReactNode;
+    post: Post | DocumentData;
 };
 
 export type Post = {
-  id: string;
-  avatar: string;
-  username: string;
-  description: string;
-  timestamp: string;
-  comments: string[];
-};
-
-export type CreatePostInitialState = {
-  id?: string;
-  avatar?: string;
-  username?: string;
-  description: string;
-  timestamp?: string;
-  comments?: string[];
+    id: string;
+    avatar: string;
+    username: string;
+    description: string;
+    timestamp: string;
+    comments: string[];
 };
 
 export type PostPageProps = {
-  isInEditMode?: boolean;
+    isInEditMode?: boolean;
 };
 
 export type MessageType = {
-  avatar: string;
-  userName: string;
-  message: string;
-  time: string;
+    avatar: string;
+    userName: string;
+    message: string;
+    time: string;
 };
