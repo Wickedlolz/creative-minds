7394baf59547cfd3d7ccdad6d68b775bc13c1e42
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-03 18:58:49 +0200

    add purpose on every one icon

--- vite.config.ts
+++ vite.config.ts
@@ -19,16 +19,19 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
                 src: '/android-chrome-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
+                purpose: 'favicon',
             },
             {
                 src: '/android-chrome-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
+                purpose: 'favicon',
             },
             {
                 src: '/apple-touch-icon.png',
                 sizes: '180x180',
                 type: 'image/png',
+                purpose: 'apple touch icon',
             },
             {
                 src: '/maskable_icon.png',
