f91b73e347882e11fb6631c162071b506ab12d8c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-03 18:09:08 +0200

    add icons for PWA ⚙️

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,24 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     devOptions: {
         enabled: true,
     },
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    includeAssets: ['vite.svg', 'masked-icon.svg'],
     manifest: {
         name: 'Creative Minds',
         short_name: 'Creative Minds',
         description: 'Place where you can share your thoughts.',
-        icons: [],
+        icons: [
+            {
+                src: '/vite.svg',
+                sizes: '192x192',
+                type: 'image/svg',
+            },
+            {
+                src: '/vite.svg',
+                sizes: '225x225',
+                type: 'image/svg',
+                purpose: 'any maskable',
+            },
+        ],
         theme_color: '#171717',
         background_color: '#e8ebf2',
         display: 'standalone',
